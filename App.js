c7e0d052cd6be9de92d18679ac6e5926b79abbc6
John Azuka <gooption@yahoo.com>
2021-01-20 21:09:44 +0200

    Update App.js

--- App.js
+++ App.js
@@ -6,13 +6,13 @@ import { AppLoading } from 'expo';
 import * as firebase from 'firebase'
 
 var firebaseConfig = {
-  apiKey: "AIzaSyCJMOf24QZuH0yO64jYsiEC2s0eDLE7-ic",
+  apiKey: "AIzaSyCJMOfxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx",
     authDomain: "transportapp-288804.firebaseapp.com",
-    databaseURL: "https://transportapp-288804.firebaseio.com",
-    projectId: "transportapp-288804",
-    storageBucket: "transportapp-288804.appspot.com",
-    messagingSenderId: "719360737864",
-    appId: "1:719360737864:web:b2e314544c77c3c6c3ce7d",
+    databaseURL: "https://transportapp-xxxxxx.firebaseio.com",
+    projectId: "transportapp-xxxxxx",
+    storageBucket: "transportapp-xxxxxx.appspot.com",
+    messagingSenderId: "xxxxxxxxxxxxxxxxxxxx",
+    appId: "1:7xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx",
     measurementId: "G-P3QQ7LM7VS"
 };
 
